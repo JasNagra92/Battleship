efd1aac9106036bf64cd73d2488356abe486d5ff
newbiejas <jaskarannagra@homail.com>
2022-08-02 22:51:24 -0700

    add receive attack function that can call ships hit method and store missed shots in an array for display

--- src/battleship.test.js
+++ src/battleship.test.js
@@ -1,5 +1,5 @@
 import { ShipFactory } from './shipfns';
-import { GameboardFactory } from './gameboard'
+import { GameboardFactory } from './gameboard';
 
 test('ShipFactory function returns ship object with length', () => {
   const ship1 = ShipFactory(3);
@@ -10,15 +10,20 @@ test('Ship created with position A4, changes position property to hit when hit',
   ship1.hit('A4');
   expect(ship1.position[0]).toEqual('hit');
 });
-test('Ship that has all positions as hit, has sunk property as true',()=>{
-    const ship1 = ShipFactory(1,['A4']);
-    ship1.hit('A4');
-    ship1.isSunk(ship1.position);
-    expect(ship1.sunk).toEqual(true)
-})
-test('gameboard recieveAttack function to call ship in arrays hit method', ()=>{
+test('Ship that has all positions as hit, has sunk property as true', () => {
+  const ship1 = ShipFactory(1, ['A4']);
+  ship1.hit('A4');
+  ship1.isSunk(ship1.position);
+  expect(ship1.sunk).toEqual(true);
+});
+test('gameboard recieveAttack function to call ship in arrays hit method', () => {
+  const myBoard = GameboardFactory();
+  myBoard.ships.push(myBoard.ShipFactory(1, ['A4']));
+  myBoard.receiveAttack('A4');
+  expect(myBoard.ships[0].position[0]).toEqual('hit');
+});
+test('gameboard stores missed shot in missed shots array', ()=>{
     const myBoard = GameboardFactory();
-    myBoard.ships.push(GameboardFactory.ShipFactory(1,"A4"));
-    myBoard.receiveAttack("A4");
-    expect(myBoard.ships[0].hit("A4")).toEqual(myBoard.ships[0].position[0]=='hit')
+    myBoard.receiveAttack('A5');
+    expect(myBoard.missedShots).toEqual(['A5'])
 })
--- src/gameboard.js
+++ src/gameboard.js
@@ -1,12 +1,23 @@
-import { ShipFactory } from "./shipfns"
+import { ShipFactory } from './shipfns';
 
-const GameboardFactory = () =>{
-    return {
-        ships:[],
-        receiveAttack(string){
-            
-        },
-        ShipFactory
-    }
-}
-export {GameboardFactory}
+const GameboardFactory = () => {
+  return {
+    ships: [],
+    missedShots: [],
+    receiveAttack(string) {
+      if (this.ships.length > 0) {
+        this.ships.forEach((ship) => {
+          if (ship.position.includes(string)) {
+            ship.hit(string);
+          } else {
+            this.missedShots.push(string);
+          }
+        });
+      } else {
+        this.missedShots.push(string);
+      }
+    },
+    ShipFactory,
+  };
+};
+export { GameboardFactory };
