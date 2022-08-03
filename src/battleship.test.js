2eeb5990910bac46ba8833ea2ffd1268f9574bde
newbiejas <jaskarannagra@homail.com>
2022-08-02 23:14:15 -0700

    tested checksunk method and create error if same coordinates entered twice

--- src/battleship.test.js
+++ src/battleship.test.js
@@ -22,8 +22,21 @@ test('gameboard recieveAttack function to call ship in arrays hit method', () =>
   myBoard.receiveAttack('A4');
   expect(myBoard.ships[0].position[0]).toEqual('hit');
 });
-test('gameboard stores missed shot in missed shots array', ()=>{
-    const myBoard = GameboardFactory();
-    myBoard.receiveAttack('A5');
-    expect(myBoard.missedShots).toEqual(['A5'])
-})
+test('gameboard stores missed shot in missed shots array', () => {
+  const myBoard = GameboardFactory();
+  myBoard.receiveAttack('A5');
+  expect(myBoard.missedShots).toEqual(['A5']);
+});
+test('gameboard checkAllSunk method should return true if all ships sunk properties are true', () => {
+  const myBoard = GameboardFactory();
+  myBoard.ships.push(myBoard.ShipFactory(1, ['A4']));
+  myBoard.receiveAttack('A4');
+  myBoard.checkAllSunk(myBoard.ships);
+  expect(myBoard.checkAllSunk(myBoard.ships)).toEqual(true);
+});
+test('gameboard returns error if shooting same coordiante twice', () => {
+  const myBoard = GameboardFactory();
+  myBoard.ships.push(myBoard.ShipFactory(1, ['A4']));
+  myBoard.receiveAttack('A3');
+  expect(()=>{myBoard.receiveAttack('A3')}).toThrow();
+});
--- src/gameboard.js
+++ src/gameboard.js
@@ -4,20 +4,34 @@ const GameboardFactory = () => {
   return {
     ships: [],
     missedShots: [],
+    previousShots: [],
     receiveAttack(string) {
-      if (this.ships.length > 0) {
+      if (this.previousShots.includes(string)) {
+        throw new Error('Coordinates have already been hit');
+      } else if (this.ships.length > 0) {
         this.ships.forEach((ship) => {
           if (ship.position.includes(string)) {
+            this.previousShots.push(string);
             ship.hit(string);
+            ship.isSunk(ship.position);
           } else {
             this.missedShots.push(string);
+            this.previousShots.push(string);
           }
         });
       } else {
         this.missedShots.push(string);
+        this.previousShots.push(string);
       }
     },
     ShipFactory,
+    checkAllSunk(array) {
+      if (array.every((ship) => ship.sunk == true)) {
+        return true;
+      } else {
+        return false;
+      }
+    },
   };
 };
 export { GameboardFactory };
