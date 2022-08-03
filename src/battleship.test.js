134b31e1006ff657638a18645486a30632fc7b93
newbiejas <jaskarannagra@homail.com>
2022-08-02 20:45:29 -0700

    write tests to check hit function is working

--- src/battleship.test.js
+++ src/battleship.test.js
@@ -1,6 +1,11 @@
-import {ShipFactory} from "./shipfns"
+import { ShipFactory } from './shipfns';
 
-test('ShipFactory function returns ship object with length', () =>{
-    const ship1 = ShipFactory(3)
-    expect(ship1).toEqual({length:3})
-})
+test('ShipFactory function returns ship object with length', () => {
+  const ship1 = ShipFactory(3);
+  expect(ship1).toMatchObject({ length: 3 });
+});
+test('Ship created with position A4, changes position property to hit when hit', () => {
+  const ship1 = ShipFactory(1, ['A4']);
+  ship1.hit('A4');
+  expect(ship1.position[0]).toEqual('hit');
+});
--- src/shipfns.js
+++ src/shipfns.js
@@ -1,20 +1,21 @@
-const ShipFactory = (length,position) => {
-    return {length,
-            position,
-            sunk: false,
-            hit(string){
-                if (position.includes(string)){
-                    const index = position.indexOf(string);
-                    position[index] = 'hit'
-                    alert('direct hit!')
-                }
-            },
-            isSunk(position){
-                if (position.every(x => x === 'hit')){
-                    this.sunk = true
-                }
-            }
-            }
-}
+const ShipFactory = (length, position) => {
+  return {
+    length,
+    position,
+    sunk: false,
+    hit(string) {
+      if (position.includes(string)) {
+        const index = position.indexOf(string);
+        position[index] = 'hit';
+        // alert('direct hit!');
+      }
+    },
+    isSunk(position) {
+      if (position.every((x) => x === 'hit')) {
+        this.sunk = true;
+      }
+    },
+  };
+};
 
-export{ShipFactory}
+export { ShipFactory };
