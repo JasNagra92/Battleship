eaece8dc0c99ba1e02709c9f2ae6fa26d21f71af
newbiejas <jaskarannagra@homail.com>
2022-08-03 23:09:16 -0700

    used EsLint for formatting correction

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,54 +1,49 @@
 import { ShipFactory } from './shipfns';
 
-const GameboardFactory = () => {
-  return {
-    ships: [],
-    missedShots: [],
-    previousShots: [],
-    board: (function makeBoard() {
-      const board = new Array(10);
-      for (let i = 0; i < 10; i++) {
-        board[i] = new Array(10);
-        for (let j = 0; j < 10; j++) {
-          board[i][j] = null;
+const GameboardFactory = () => ({
+  ships: [],
+  missedShots: [],
+  previousShots: [],
+  board: (function makeBoard() {
+    const board = new Array(10);
+    for (let i = 0; i < 10; i += 1) {
+      board[i] = new Array(10);
+      for (let j = 0; j < 10; j += 1) {
+        board[i][j] = null;
+      }
+    }
+    return board;
+  }()),
+  receiveAttack(x, y) {
+    this.previousShots.forEach((coordinates) => {
+      if (coordinates[0] === x && coordinates[1] === y) {
+        throw new Error('coordinates already hit');
+      }
+    });
+    if (this.ships.length > 0) {
+      this.ships.forEach((ship) => {
+        if (
+          ship.position.forEach((coordinates) => {
+            if (coordinates[0] === x && coordinates[1] === y) {
+              ship.hit(x, y);
+              ship.isSunk(ship.position);
+            }
+          })
+        ) {
+          this.previousShots.push([x, y]);
+        } else {
+          this.missedShots.push([x, y]);
+          this.previousShots.push([x, y]);
         }
-      }
-      return board;
-    })(),
-    receiveAttack(x, y) {
-      if (
-        this.previousShots.forEach(function (coordinates) {
-          if (coordinates[0] == x && coordinates[1] == y) {
-            throw new Error('coordinates already hit');
-          }
-        })
-      ) {
-      } else if (this.ships.length > 0) {
-        this.ships.forEach((ship) => {
-          if (
-            ship.position.forEach(function (coordinates) {
-              if (coordinates[0] == x && coordinates[1] == y) {
-                ship.hit(x, y);
-                ship.isSunk(ship.position);
-              }
-            })
-          ) {
-            this.previousShots.push([x, y]);
-          } else {
-            this.missedShots.push([x, y]);
-            this.previousShots.push([x, y]);
-          }
-        });
-      }
-    },
-    ShipFactory,
-    checkAllSunk(array) {
-      if (array.every((ship) => ship.sunk === true)) {
-        return true;
-      } else {
-        return false;
-      }
-    },
-  };
-};
+      });
+    }
+  },
+  ShipFactory,
+  checkAllSunk(array) {
+    if (array.every((ship) => ship.sunk === true)) {
+      return true;
+    }
+    return false;
+  },
+});
 export { GameboardFactory };
--- src/shipfns.js
+++ src/shipfns.js
@@ -1,21 +1,21 @@
-const ShipFactory = (length, position) => {
-  return {
-    length,
-    position,
-    sunk: false,
-    hit(x, y) {
-      this.position.forEach(function (coordinates,index) {
-        if (coordinates[0] == x && coordinates[1] == y) {
-          position[index] = "hit";
-        }
-      });
-    },
-    isSunk(array) {
-      if (array.every((x) => x === 'hit')) {
-        this.sunk = true;
+/* eslint-disable import/prefer-default-export */
+const ShipFactory = (length, position) => ({
+  length,
+  position,
+  sunk: false,
+  hit(x, y) {
+    this.position.forEach((coordinates, index) => {
+      if (coordinates[0] === x && coordinates[1] === y) {
+        // eslint-disable-next-line no-param-reassign
+        position[index] = 'hit';
       }
-    },
-  };
-};
+    });
+  },
+  isSunk(array) {
+    if (array.every((x) => x === 'hit')) {
+      this.sunk = true;
+    }
+  },
+});
 
 export { ShipFactory };
