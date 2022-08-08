ab6b8ed9a000323340b122adb6dd45391397acfb
newbiejas <jaskarannagra@homail.com>
2022-08-08 11:41:37 -0700

    cpu generate fleet function generates 5 ships with random coordinates and pushes ships to boards ship array

--- src/playerfactory.js
+++ src/playerfactory.js
@@ -1,3 +1,11 @@
+/* eslint-disable no-unused-expressions */
+import {
+  getRandomInt,
+  randomHorizontalCoordinates,
+  randomVerticalCoordinates,
+} from './helpers';
+import { ShipFactory } from './shipfns';
+
 const Player = (name) => ({
   name,
   shots: [],
@@ -21,6 +29,82 @@ const CpuPlayer = () => ({
     });
     gameboard.receiveAttack(x, y, target);
     this.shots.push([x, y]);
+  },
+  generateFleet() {
+    let currentShipCoordinates = [[0, 0]];
+    for (let i = 2; i <= 5; i += 1) {
+      if (i === 2) {
+        const directionNumber = getRandomInt(2);
+        let direction;
+        directionNumber === 0
+          ? (direction = 'horizontal')
+          : (direction = 'vertical');
+        let coordinates;
+        if (direction === 'horizontal') {
+          coordinates = randomHorizontalCoordinates(2, currentShipCoordinates);
+        } else if (direction === 'vertical') {
+          coordinates = randomVerticalCoordinates(2, currentShipCoordinates);
+        }
+        this.gameboard.ships.push(ShipFactory(2, coordinates));
+        currentShipCoordinates.push(coordinates);
+      } else if (i === 3) {
+        let directionNumber = getRandomInt(2);
+        let direction;
+        directionNumber === 0
+          ? (direction = 'horizontal')
+          : (direction = 'vertical');
+        let coordinates;
+        if (direction === 'horizontal') {
+          coordinates = randomHorizontalCoordinates(3, currentShipCoordinates);
+        } else if (direction === 'vertical') {
+          coordinates = randomVerticalCoordinates(3, currentShipCoordinates);
+        }
+        this.gameboard.ships.push(ShipFactory(3, coordinates));
+        currentShipCoordinates.push(coordinates);
+
+        directionNumber = getRandomInt(2);
+        directionNumber === 0
+          ? (direction = 'horizontal')
+          : (direction = 'vertical');
+        coordinates;
+        if (direction === 'horizontal') {
+          coordinates = randomHorizontalCoordinates(3, currentShipCoordinates);
+        } else if (direction === 'vertical') {
+          coordinates = randomVerticalCoordinates(3, currentShipCoordinates);
+        }
+        this.gameboard.ships.push(ShipFactory(3, coordinates));
+        currentShipCoordinates.push(coordinates);
+      } else if (i === 4) {
+        const directionNumber = getRandomInt(2);
+        let direction;
+        directionNumber === 0
+          ? (direction = 'horizontal')
+          : (direction = 'vertical');
+        let coordinates;
+        if (direction === 'horizontal') {
+          coordinates = randomHorizontalCoordinates(4, currentShipCoordinates);
+        } else if (direction === 'vertical') {
+          coordinates = randomVerticalCoordinates(4, currentShipCoordinates);
+        }
+        this.gameboard.ships.push(ShipFactory(4, coordinates));
+        currentShipCoordinates.push(coordinates);
+      } else if (i === 5) {
+        const directionNumber = getRandomInt(2);
+        let direction;
+        directionNumber === 0
+          ? (direction = 'horizontal')
+          : (direction = 'vertical');
+        let coordinates;
+        if (direction === 'horizontal') {
+          coordinates = randomHorizontalCoordinates(5, currentShipCoordinates);
+        } else if (direction === 'vertical') {
+          coordinates = randomVerticalCoordinates(5, currentShipCoordinates);
+        }
+        this.gameboard.ships.push(ShipFactory(5, coordinates));
+        currentShipCoordinates.push(coordinates);
+        console.log(currentShipCoordinates)
+      }
+    }
   },
 });
 export { Player, CpuPlayer };
